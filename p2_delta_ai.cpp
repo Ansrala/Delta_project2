941c3f06e9e32d601de06f51d473e126efe87708
Westin-Mac <westin007@gmail.com>
2013-03-10 16:37:46 -0500

    in case something happens

--- p2_delta_ai.cpp
+++ p2_delta_ai.cpp
@@ -7,7 +7,6 @@
 #include <string>
 #include <std_msgs/String.h>
 
-
 //our custom messages
 #include <Delta_project2/lineList.h>
 #include <Delta_project2/pointList.h>
@@ -21,8 +20,12 @@ Delta_project2::pointList obstacles;
 
 string currState;
 
+void checkSensorChange(const serializer::SensorState& msg);
 void checkObstacleChange(const Delta_project2::pointList& msg);
-
+//geometry_msgs::Twist passThroughDoor();
+geometry_msgs::Twist avoidObstacle();
+geometry_msgs::Twist wander();
+float getValue();
 void checkWallChange(const Delta_project2::lineList& msg);
 
 int main(int argc, char **argv)
@@ -35,6 +38,8 @@ int main(int argc, char **argv)
   ros::NodeHandle ss;
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
 
+  geometry_msgs::Twist output;
+
   //the state messenger doesn't need the same resolution as the cmd::velocity channel.  
 //Thus, we only publish 5 times a second.
   ros::Publisher cmd_vel_pub2 = s.advertise<geometry_msgs::Twist>("FState", 5);
@@ -46,22 +51,26 @@ ros::topic::waitForMessage<nav_msgs::Odometry>(std::string("odom"), n,ros::Durat
    //we listen a little faster than we publish, since we don't know when it will change
   	ros::Subscriber suba = ls.subscribe("worldinfoPoints", 10, checkObstacleChange);
 	ros::Subscriber subb = rs.subscribe("worldinfoLines", 10, checkWallChange);
-        ros::Subscriber subc = ss.subscribe("sensors", 10, checkSensorChange);
+        ros::Subscriber subc = ss.subscribe("serializer_node/sensors", 10, checkSensorChange);
 
    
 //begin your methodology
-/*
-while()
+
+while(ros::ok())
 {
 	//testing stuff
-
+	output = wander(); //avoidObstacle();
+/*
 	if()  //safety dance
 	{}
 	else if() //door...stuff
 	{}
-	//etc
-}
 */
+	//etc
+	cmd_vel_pub.publish(output);
+	ros::spinOnce();
+}
+
 
 //end your methadology
 
@@ -87,41 +96,45 @@ void checkWallChange(const Delta_project2::lineList& msg)
 {
   	for(int i = 0; i < msg.x1.size(); i++)
 	{
-		wall.x1.push_back(msg.x1[i]);
-		wall.y1.push_back(msg.y1[i]);
-		//wall.z1.push_back(msg.z1[i]);
+		walls.x1.push_back(msg.x1[i]);
+		walls.y1.push_back(msg.y1[i]);
+		//walls.z1.push_back(msg.z1[i]);
 
-		wall.x2.push_back(msg.x2[i]);
-		wall.y2.push_back(msg.y2[i]);
-		//wall.z2.push_back(msg.z2[i]);
+		walls.x2.push_back(msg.x2[i]);
+		walls.y2.push_back(msg.y2[i]);
+		//walls.z2.push_back(msg.z2[i]);
 	}
 }
 
-void checkSensorChange(const Serializer::SensorState& msg)
+void checkSensorChange(const serializer::SensorState& msg)
 {
   
 }
 
 geometry_msgs::Twist avoidObstacle()
 {
-/*
-//assume currstate will have local location for obstacles (x,y,r)
-//know bounds for walls (x1, y1) (x2, y2)
-//assume floats
+
 geometry_msgs::Twist msg;
 //float distance;
 //distance = sqrt((pow(obstacleX,2) + pow(obstacleY,2)));
-	//if( x1<0 && x2<0)//wall is entirely to left of robot
-	//{
-		if(obstacleY > 0)
+bool wallToLeft = false;
+bool wallInFront = false;
+for(int i = 0; i < walls.x1.size();i++){
+	if(walls.x1[i] < 0 && walls.x2[i] < 0)
+		wallToLeft = true;
+	if(walls.x1[i] > 20 && walls.x2[i]<0)
+		wallInFront = true;
+}
+  
+		if(obstacles.y[0] > 0)
 		{
-		 	if(obstacleX > -16 && obstacleX < 12){
+		 	if(obstacles.x[0] > -16 && obstacles.x[0] < 12){
 				
 				//veer right
 				msg.linear.x = 0.25;
 				msg.angular.z = -0.25;
 			}
-			else if( obstacleX > 12 && obstacleX < 16){
+			else if( obstacles.x[0] > 12 && obstacles.x[0] < 16){
 				//veer left slightly
 				msg.linear.x = 0.25;
 				msg.angular.z = 0.1;
@@ -129,14 +142,14 @@ geometry_msgs::Twist msg;
 			else
 				msg.linear.x = 0.25;	
 		}
-		else if(){
-			if(obstacleX > -16 && obstacleX < 12){
+		else if(!wallInFront){
+			if(obstacles.x[0] > -16 && obstacles.x[0] < 12){
 				//veer left
 				msg.linear.x = 0.25;
 				msg.angular.z = 0.25;
 
 			}
-			else if( obstacleX > 12 && obstacleX < 16){
+			else if( obstacles.x[0] > 12 && obstacles.x[0] < 16){
 				//veer right slightly
 				msg.linear.x = 0.25;
 				msg.angular.z = -0.1;
@@ -150,24 +163,24 @@ geometry_msgs::Twist msg;
 			msg.angular.z = -0.50;
 		}
 
-	//}
+	
 	return msg;
-*/
+
 }
 
-geometry_msgs::Twist passThroughDoor()
-{
+//geometry_msgs::Twist passThroughDoor()
+//{
 
 //assume currstate will have local location for obstacles (x,y,r)
 //know bounds for walls (x1, y1) (x2, y2)
 //assume floats
-}
+//}
 
 geometry_msgs::Twist wander()
 {
-/*
+
   geometry_msgs::Twist msg;
-  float32 value = getValue();
+  float value = getValue();
   if(value<=1.75)//I'm too close to the wall
   {
     msg.linear.x = .25;
@@ -190,16 +203,16 @@ geometry_msgs::Twist wander()
   return msg;
 }
 
-float32 getValue()
+float getValue()
 {
-  serializer::sensor msg;
-  for(int i = 0; msg <=5; i++)
+  serializer::SensorState msg;
+  for(int i = 0; i <=5; i++)
   {
     if(msg.name[i] == "left_ir")
     {
       return msg.value[i];
     }
-  }*/
+  }
 }
 
 
