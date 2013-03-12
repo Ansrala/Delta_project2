f872a29a28ca40b106e8e67e963fd7ee792fcb22
Westin Breger <wbreger@eb2029-06.(none)>
2013-03-12 03:31:55 -0500

    fuck everything

--- sensors.cpp
+++ sensors.cpp
@@ -2,10 +2,10 @@
 //interprets data from sensors
 //posts them to "worldinfo" topic
 
-#define ANGLE_RES 10
-#define SKIP_MAX 9
-#define THRESHOLD .4f
-
+#define ANGLE_RES 7
+#define OB_SIZE 7
+#define THRESHOLD .02f
+#define MIN_POINT_COUNT 5
 #define PI 3.14592f
 
 #include "ros/ros.h"
@@ -16,8 +16,8 @@
 #include <sensor_msgs/LaserScan.h>
 #include <stdlib.h>
 #include <cmath>
-//#include <iostream>
-//#include <vector>
+#include <iostream>
+#include <vector>
 
 //our custom messages
 #include <p2_delta/lineList.h>
@@ -32,7 +32,7 @@ void loadLaser(const sensor_msgs::LaserScan& msg);
 
 void checkStateChange(const std_msgs::String& msg);
 
-void cleanLinesOut(p2_delta::lineList* linesOut);
+void cleanLinesOut(p2_delta::lineList &linesOut, p2_delta::pointList &pointsOut);
 
 int main(int argc, char **argv)
 {
@@ -44,7 +44,7 @@ int main(int argc, char **argv)
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
 
 
-ros::topic::waitForMessage<nav_msgs::Odometry>(string("odom"), n,ros::Duration(30));
+//ros::topic::waitForMessage<nav_msgs::Odometry>(string("odom"), n,ros::Duration(30));
     geometry_msgs::Twist msg;
 
 
@@ -68,43 +68,23 @@ for(int i = 0; i < ANGLE_RES; i++)
 
   
    ros::Subscriber info_get = sense.subscribe("scan", 500, loadLaser);
-   ros::Publisher info_pub1 = talk.advertise<p2_delta::pointList>("worldinfoPoints", 500);
-   ros::Publisher info_pub2 = talk.advertise<p2_delta::lineList>("worldinfoLines", 500);
+   ros::Publisher info_pub1 = talk.advertise<p2_delta::pointList>("worldinfoPoints", 50);
+   ros::Publisher info_pub2 = talk.advertise<p2_delta::lineList>("worldinfoLines", 50);
 
    p2_delta::lineList linesOut;
    p2_delta::pointList pointsOut;
    
 //begin your methodology
 
-//ok.
-//so.
-//A Hough transform transforms a set of points into a set of lines.
-//as seen here:
-//http://en.wikipedia.org/wiki/Hough_transform
 
-//basically, for each point, You draw a bunch of standard lines over it, as defined by you
-//for here, it is defined as ANGLE_RES, where the robot will check each point ANGLE_RES times, against each point 
-//Where each line is an angle 180/ANGLE_RES
-//for each line, a perpendicular line is drawn from your defined line to the orgin, and the distance measured
-//these results are stored in a matrix angleOfTestLine to distance measured
-
-//if your matrix has a set of rows or partial rows who have similar (within a thresh-hold) distances, the referring points lock to your test line,
-//and can be considered a line
-
-//other notes:
-//perpendicular vectors can be found by switching x and y coords once centered on orgin
-//slope = (y2-y1)/(x2-x1)
-//normal slope = -(y2-y1)/(x2-x1) or (y2-y1)/-(x2-x1)
-//y1 = slope(x1) + c
-//c = y1 - slope(x1)
-
+int testCount = 0;
 while(ros::ok())
 {
 
 float* linearcoordX = new float[now.ranges.size()];
 float* linearcoordY = new float[now.ranges.size()];
 
-cleanLinesOut(&linesOut);
+cleanLinesOut(linesOut, pointsOut);
 
 //convert to linear coords here
 //ROS_INFO("making linear matrix");
@@ -115,129 +95,104 @@ for (int i = 0; i < now.ranges.size(); i++)
 
 }
 
-//end conversion
+//slope of i and i+1
+//float slope;
+bool stillwall = false;
+float distanceSquared;
+int tempPointCount = 0;
+int skippedPointCount = 0;
+float currSlope = 0;
+float nextSlope = 0;
+float endpoint[2];
+float startpoint[2];
 
-
-//make distance matrix
-float* distMatrix[ANGLE_RES];
-for(int i = 0; i < ANGLE_RES; i++)
-	distMatrix[i] = new float[now.ranges.size()];
-float Xcross;
-float Ycross;
-float c1;
-
-//ROS_INFO("populating linear matrix");
-for(int i = 0; i < now.ranges.size(); i++)
+for (int i = 0; i < now.ranges.size(); i++)
 {
-	for(int j = 0; j < ANGLE_RES; j++)
-	{
-		//find y-intercept
-		//c1 = y1 - mx1
-		c1 = linearcoordY[i] - linearcoordX[i] * slope[j];
-
-		//find the common X coord
-		Xcross = c1 / diffm1m2[j];
-		Ycross = slope[j] * Xcross + c1;
-
-		distMatrix[j][i] = sqrt(Xcross * Xcross + Ycross * Ycross);
+ROS_INFO("i: %d",i);
+	//checking for bad points
+	if( now.ranges[i] < 0.01 || now.ranges[i] >4){
+		skippedPointCount++;
+		tempPointCount++;
+		continue;
 	}
-}
+	//not first point
+	if(i > 0 )
+	{	
 
-//compare each column, note points that have similar distances
-float last = -50;
-bool onAline =  false;
-int skips = 0;
-float lastHitX = 0;
-float lastHitY = 0;
-
-//ROS_INFO("parsing Hough transform matrix");
-for(int j = 0; j < now.ranges.size(); j++)
-{
-	last = distMatrix[0][0];
-	for(int i = 0; i < ANGLE_RES; i++)
-	{   
-		//ROS_INFO("j: %d , i: %d, Value: %f" , j, i,distMatrix[i][j]);
-		// is like the last point
-		if(distMatrix[i][j] > (last - THRESHOLD) && distMatrix[i][j] < (last + THRESHOLD) && !(distMatrix[i][j] < .1f) && j > 0  && (distMatrix[i][j] > 3.0f))
+		//check distance between points
+		distanceSquared = pow(linearcoordX[i]-linearcoordX[i-(1+skippedPointCount)],2) + pow(linearcoordY[i] - linearcoordY[i-(1+skippedPointCount)],2);
+	    if(distanceSquared < pow(0.01 * (skippedPointCount+1),2))
 		{
-			//new line
-			if(!onAline)
+			//new wall
+			if(!stillwall)
 			{
-				//ROS_INFO("starting new line...");
-				onAline = true;
-				skips = 0;
-				//create new line	
-
-				linesOut.x1.push_back(linearcoordX[j-1]);
-				linesOut.y1.push_back(linearcoordY[j-1]);
-
-				linesOut.x2.push_back(linearcoordX[j]);
-				linesOut.y2.push_back(linearcoordY[j]);
-
-				lastHitX = linesOut.x2.back();
-				lastHitY = linesOut.y2.back();
-			}
-			else 
-			{
-				//ROS_INFO("continuing line...");
-				skips = 0;
-				linesOut.x2.back() = linearcoordX[j];
-				linesOut.y2.back() = linearcoordY[j];
-
-				lastHitX = linesOut.x2.back();
-				lastHitY = linesOut.y2.back();
-			
-			}
-				
-			//ROS_INFO("moving on...");
-		}
-		//strayed from the line
-		else
-		{
-			if(onAline)
-			{
-				//ROS_INFO("point skip");
-				skips++;
-				//grace period
-				if(skips < SKIP_MAX)
-				{
-					//add anyway
-					linesOut.x2.back() = linearcoordX[j];
-					linesOut.y2.back() = linearcoordY[j];
-				}
-				//new line
-				else
-				{
-					//ROS_INFO("ending line");
-					 
-					onAline = false;
-					linesOut.x2.back() = lastHitX;
-					linesOut.y2.back() = lastHitY;
-					//ROS_INFO("x1: %f, y1: %f, x2: %f, y2: %f", linesOut.x1.back(), linesOut.y1.back(), linesOut.x2.back(), linesOut.y2.back()); 
-
-					for(int k = 0; k < skips; k++)
-					{
-						pointsOut.x.push_back(linearcoordX[j-k]);
-						pointsOut.y.push_back(linearcoordY[j-k]);
+				//if current point is still in range (accounting for skips...)
+				if(tempPointCount < MIN_POINT_COUNT + skippedPointCount){
+					tempPointCount++;
+					//if we are ready to check slope, log slope
+					if(tempPointCount == MIN_POINT_COUNT + skippedPointCount){
+						currSlope = (linearcoordY[i] - linearcoordY[i-tempPointCount])/(linearcoordX[i] - linearcoordX[i-tempPointCount]);
+						stillwall = true;
+						endpoint[0] = linearcoordX[i];
+						endpoint[1] = linearcoordY[i];
+						startpoint[0] = linearcoordX[i-tempPointCount];
+						startpoint[1] = linearcoordY[i-tempPointCount];
 					}
 				}
 			}
+			//adding to wall
+			else
+			{
+				//test the slope against the slope for the new point
+				nextSlope = (linearcoordY[i] - endpoint[1])/(linearcoordX[i] - endpoint[0]);
+				if(nextSlope > (currSlope - THRESHOLD) && nextSlope < (currSlope + THRESHOLD))
+				{
+					endpoint[0] = linearcoordX[i];		
+					endpoint[1] = linearcoordY[i];
+				}
+				else
+				{
+					stillwall = false;
+					linesOut.x1.push_back(startpoint[0]);
+					linesOut.y1.push_back(startpoint[1]);
+					linesOut.x2.push_back(endpoint[0]);
+					linesOut.y2.push_back(endpoint[1]);
+				}
+			}
+
 		}
-		last = distMatrix[i][j];
+		else
+		{
+			if(stillwall)
+			{
+				linesOut.x1.push_back(startpoint[0]);
+				linesOut.y1.push_back(startpoint[1]);
+				linesOut.x2.push_back(endpoint[0]);
+				linesOut.y2.push_back(endpoint[1]);
+			}
+			stillwall = false;
+
+			//since this point is not a wall, it is an obstacle
+			pointsOut.x.push_back(linearcoordX[i]);
+			pointsOut.y.push_back(linearcoordY[i]);
+		}
+		tempPointCount = 0;
+		skippedPointCount = 0;
+			
 		
+
 	}
-	
+		//record slope
+		//end conversion
+
 }
-info_pub1.publish(pointsOut);
-info_pub2.publish(linesOut);
 
-//ROS_INFO("cleaning...");
-delete [] linearcoordY;
-delete [] linearcoordX;
-for(int i = 0; i < ANGLE_RES; i++)
-	delete [] distMatrix[i];
+info_pub1.publish(linesOut);
+info_pub2.publish(pointsOut);
 
-ROS_INFO("NUMBER OF LINES: %d ", linesOut.x1.size());
+
+//ROS_INFO("ENDING LOOP Count: %d", testCount);
+//testCount++;
 ros::spinOnce();
 
 }
@@ -275,12 +230,15 @@ void loadLaser(const sensor_msgs::LaserScan& msg)
 
 }
 
-void cleanLinesOut(p2_delta::lineList* linesOut)
+void cleanLinesOut(p2_delta::lineList &linesOut, p2_delta::pointList &pointsOut)
 {
-	linesOut->x1.erase(linesOut->x1.begin(), linesOut->x1.end());
-	linesOut->y1.erase(linesOut->y1.begin(), linesOut->y1.end());
-	linesOut->x2.erase(linesOut->x2.begin(), linesOut->x2.end());
-	linesOut->y2.erase(linesOut->y2.begin(), linesOut->y2.end());
+	linesOut.x1.erase(linesOut.x1.begin(), linesOut.x1.end());
+	linesOut.y1.erase(linesOut.y1.begin(), linesOut.y1.end());
+	linesOut.x2.erase(linesOut.x2.begin(), linesOut.x2.end());
+	linesOut.y2.erase(linesOut.y2.begin(), linesOut.y2.end());
+
+	pointsOut.x.erase(pointsOut.x.begin(), pointsOut.x.end());
+	pointsOut.y.erase(pointsOut.y.begin(), pointsOut.y.end());
 }
 
 
